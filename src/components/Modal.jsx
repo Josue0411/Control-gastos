d74e75002f328c3ac8c307e4667aa18d1fc3caca
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-21 22:15:40 -0400

    mostrando y ocultando modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import CerrarModal from '../img/cerrar.svg'
+const Modal = ({setModal}) => {
+    const ocultarModal= ()=>{
+        setModal(false)
+    }
+  return (
+    <div className="modal">
+<div className="cerrar-modal">
+    <img src={CerrarModal} alt="Cerrar modal" onClick={ocultarModal} />
+
+</div>
+    </div>
+  )
+}
+
+export default Modal
