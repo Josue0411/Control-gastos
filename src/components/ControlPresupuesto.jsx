af88c18334cbf7f01a3f9dce0630a7347fcb3c43
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-24 10:52:30 -0400

    personalizar la grafica circular

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 const ControlPresupuesto = ({ presupuesto, gastos }) => {
   const [porcentaje, setPorcentaje] = useState(0);
@@ -32,7 +32,13 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <CircularProgressbar value={porcentaje} />
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: "#3B82F6",
+            trailColor: "#F5F5F5",
+          })}
+          value={porcentaje}
+        />
       </div>
       <div className="contenido-presupuesto">
         <p>
